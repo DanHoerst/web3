7da780a3459a1f8a5af24a94e3fad08fadc5a91e
Dan Hoerst <dhoerst1@gmail.com>
2021-11-23 12:38:50 -0500

    Convert initial supply to wei

--- demos/erc20-brownie/scripts/deploy.py
+++ demos/erc20-brownie/scripts/deploy.py
@@ -1,15 +1,18 @@
 from brownie import config, HoerstToken, network
 from scripts.helpful_scripts import get_account
+from web3 import Web3
+
+initial_supply = Web3.toWei(1000, "ether")
 
 def deploy_erc20():
     account = get_account()
-    initial_supply = 10000
-    lottery = HoerstToken.deploy(
+
+    token = HoerstToken.deploy(
         initial_supply,
         {"from": account},
         publish_source=config["networks"][network.show_active()].get("verify", False)
     )
-    account_balance = lottery.balanceOf(account)
+    account_balance = token.balanceOf(account)
     print(f"{account} balance is {account_balance}")
 
 def main():
